243c73278ea35f5fb020cba92bdb8f1b406ac6fc
Hector Berrutti <82546028+TitoBerru@users.noreply.github.com>
2024-07-26 18:59:00 -0300

    proyecto final d practica react con vite y tailwindcss

---
+++ src/data/task.js
@@ -0,0 +1,19 @@
+export const tasks =[
+    {
+        id:0,
+        title:"mi primer tarea",
+        description: 'mi primer tarear'
+    },
+    
+    {
+        id:1,
+        title:"mi segunda tarea",
+        description: 'mi segunda tarear'
+    },
+    
+    {
+        id:2,
+        title:"mi tercer tarea",
+        description: 'mi tercer tarear'
+    }
+]
